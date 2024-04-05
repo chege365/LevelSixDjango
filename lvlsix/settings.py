b4347c1ae6f1a2671503dbaf412ddee4d46c452c
stepboy101 <chegestephen101@gmail.com>
2024-04-05 17:10:50 +0100

    Commit Changes

--- lvlsix/settings.py
+++ lvlsix/settings.py
@@ -136,3 +136,5 @@ CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+ALLOWED_HOSTS = ['employee-dev.azurewebsites.net', 'www.employee-dev.azurewebsites.net']
